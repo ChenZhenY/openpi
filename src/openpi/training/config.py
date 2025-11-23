8ca98e2fd6e1d86d6215a90945db6c3316c49535
Zhenyang Chen <chenzy086@gmail.com>
2025-11-23 10:57:31 -0500

    config for interpolation1122 ICE

--- src/openpi/training/config.py
+++ src/openpi/training/config.py
@@ -1153,7 +1153,8 @@ _CONFIGS = [
         # Each dataset uses its own prompt handling if prompt_from_task is True.
         model=pi0_config.Pi0Config(pi05=True, action_horizon=10, discrete_state_input=False),
         data=MultiDatasetLiberoDataConfig(
-            repo_ids=["pi_libero_lerobot", "lerobot_interpolation_30steps_1112"], 
+            # repo_ids=["pi_libero_lerobot", "lerobot_interpolation_50steps_1112"],
+            repo_ids=["pi_libero_lerobot", "lerobot_interpolation_50steps_inter10_1122"],
             # repo_ids=["pi_libero_lerobot", "lerobot_all_task_pairs_step_30_1107_lang"],  # Example: cotrain on multiple datasets
             # Optional: adjust sampling ratio. [2.0, 1.0] means dataset 0 is sampled twice as often as dataset 1.
             # If None, uniform sampling is used.
@@ -1205,6 +1206,42 @@ _CONFIGS = [
         weight_loader=weight_loaders.CheckpointWeightLoader("gs://openpi-assets/checkpoints/pi05_libero/params"),
         save_interval=2000,
         keep_period=2000,
+        lr_schedule=_optimizer.CosineDecaySchedule(
+            warmup_steps=10_000,
+            peak_lr=5e-5,
+            decay_steps=1_000_000,
+            decay_lr=5e-5,
+        ),
+        optimizer=_optimizer.AdamW(clip_gradient_norm=1.0),
+        ema_decay=0.999,
+    ),
+    
+TrainConfig(
+        name="pi05_libero_cotraining_interpolation_1122",
+        # Example config demonstrating cotraining on multiple Libero datasets.
+        # All datasets share the same transforms and normalization stats (from asset_id).
+        # Each dataset uses its own prompt handling if prompt_from_task is True.
+        model=pi0_config.Pi0Config(pi05=True, action_horizon=10, discrete_state_input=False),
+        data=MultiDatasetLiberoDataConfig(
+            # repo_ids=["pi_libero_lerobot", "lerobot_interpolation_50steps_1122"], 
+            repo_ids=["pi_libero_lerobot", "lerobot_interpolation_50steps_inter5_1122"],
+            # repo_ids=["pi_libero_lerobot", "lerobot_all_task_pairs_step_30_1107_lang"],  # Example: cotrain on multiple datasets
+            # Optional: adjust sampling ratio. [2.0, 1.0] means dataset 0 is sampled twice as often as dataset 1.
+            # If None, uniform sampling is used.
+            dataset_weights=[1.0, 3.0],
+            base_config=DataConfig(prompt_from_task=True),
+            extra_delta_transform=False,
+            # Use shared normalization stats from a single asset_id
+            assets=AssetsConfig(
+                assets_dir="/home/hice1/zchen927/scratch/openpi/assets/pi05_libero/physical-intelligence",
+                asset_id="libero",
+            ),
+        ),
+        batch_size=256,
+        num_train_steps=500,
+        weight_loader=weight_loaders.CheckpointWeightLoader("gs://openpi-assets/checkpoints/pi05_libero/params"),
+        save_interval=500,
+        keep_period=500,
         lr_schedule=_optimizer.CosineDecaySchedule(
             warmup_steps=10_000,
             peak_lr=5e-5,
